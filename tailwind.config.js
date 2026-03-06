a95752b2783291e89707956b3ef1b863b2264889
JIN0130-dev <w.bexpert5007@gmail.com>
2026-03-06 21:28:04 +0900

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1DA1F2",
+        dark: "#0F0F0F",
+        light: "#F5F5F5"
+      }
+    },
+  },
+  plugins: [],
+}
